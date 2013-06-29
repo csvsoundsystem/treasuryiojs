f0882773de1c77e27c9c82206f1c0bde4541351b
Thomas Levine <occurrence@thomaslevine.com>
2013-06-29 13:29:52 -0700

    package it

---
+++ readme.md
@@ -0,0 +1,12 @@
+treasuryiojs
+=====
+This is a [treasury.io](http://treasury.io) client for JavaScript.
+
+## Install
+
+    npm install treasuryio
+
+## Run
+
+    var treasuryio = require('treasuryio')
+    var result = treasuryio('SELECT * FROM t1 LIMIT 10;')
---
+++ treasuryio.js
@@ -0,0 +1,21 @@
+module.exports = (function(){
+  var $   = require('jquery'),
+      dsv = require('dsv'),
+      pi_endpoint = 'https://premium.scraperwiki.com/cc7znvq/47d80ae900e04f2/sql/?q=';
+
+  function convertJSONtoCSV(query){
+    fetchJSON(query).done(function(json){
+      $download_csv_btn.html('Download .csv')
+      var csv = dsv.csv.format(json);
+      console.log(csv)
+    });
+  };
+   
+  function fetchJSON(query){
+    return $.ajax({
+      url: api_endpoint + query
+    });
+  };
+
+  return convertJSONtoCSV;
+})()
